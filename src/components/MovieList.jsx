d375de85fe572f6ec0f1d744ae6c42dc64cf63ad
hasungpil <dependspic@gmail.com>
2024-01-16 16:11:48 +0900

    id별호출테스트중

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,34 +3,58 @@ import React, { useCallback, useEffect, useState } from "react"; // eslint-disab
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
+  const [movie1, setMovie1] = useState([]);
 
-  const APIKEY = "https://omdbapi.com/?apikey=ef297970&s=movie";
+  const APIKEY = "https://omdbapi.com/?apikey=ef297970&s=vanilla";
+  const MOVIE1 = "https://omdbapi.com/?apikey=ef297970&i=tt0816692";
+  const MovieId = [
+    "tt1798709",
+    "tt0206634",
+    "tt2194499",
+    "tt0816711",
+    "tt1375666",
+    "tt2543164",
+    "tt0816692",
+    "tt2582802",
+    "tt0359950",
+    "tt0770828",
+    "tt0259711",
+  ];
+
   const fetchMovies = useCallback(async () => {
     const response = await axios.get(APIKEY);
     setMovies(response.data.Search);
   }, [APIKEY]);
 
+  const fetchID = useCallback(async () => {
+    const response = await axios.get(MovieId);
+    setMovie1(response.data);
+    console.log(response.data);
+  }, [MovieId]);
+
   useEffect(() => {
     fetchMovies();
-  }, [fetchMovies]);
+    fetchID();
+  }, [fetchMovies, fetchID]);
 
   return (
     <>
       <div className="m0auto">
         <ul className="movie-list">
           {movies.map((movie) => {
-            console.log(movie.Year);
-            <li key={movie.imdbID}>
-              <a href="">
-                <div className="movie-list__info">
-                  <div className="movie-list__poster">
-                    <img src={movie.Poster} alt="" />
+            return (
+              <li key={movie.imdbID} id={movie.imdbID}>
+                <a href="">
+                  <div className="movie-list__info">
+                    <div className="movie-list__poster">
+                      <img src={movie.Poster} alt="" />
+                    </div>
+                    <div className="movie-list__title">{movies.Title}</div>
+                    <div className="movie-list__year">{movies.Year}</div>
                   </div>
-                  <div className="movie-list__title">{movies.Title}</div>
-                  <div className="movie-list__year">{movies.Year}</div>
-                </div>
-              </a>
-            </li>;
+                </a>
+              </li>
+            );
           })}
         </ul>
       </div>
