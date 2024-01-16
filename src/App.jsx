996f015986088df704f0c7e8d420a9128a6d0c05
hasungpil <dependspic@gmail.com>
2024-01-16 11:27:53 +0900

    styleguide

--- src/App.jsx
+++ src/App.jsx
@@ -79,6 +79,10 @@ function App() {
           <h2 className="heading small">heading small</h2>
           <p className="txt-type1">txt-type1 txt-type1 txt-type1</p>
           <p className="txt-type2">txt-type2 txt-type2 txt-type2</p>
+          <h3 className="sub-title">
+            <strong>SUB TITLE</strong>
+            <span>SUB TITLE TEXT</span>
+          </h3>
 
           {/* 버튼 */}
           <div className="mt30"></div>
@@ -184,10 +188,79 @@ function App() {
             </li>
           </ul>
 
+          <h2 className="heading small  mt30">Favorite View</h2>
+          <div className="mt10"></div>
+          <ul className="movie-list fav">
+            <li>
+              <a href="">
+                <div className="movie-list__info">
+                  <div className="movie-list__poster">
+                    <img
+                      src="https://demo.gloriathemes.com/noxe/demo/wp-content/uploads/2020/02/title-image-12-1255x790.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="movie-list__title">제목</div>
+                  <div className="movie-list__genre">Adventure, Drama, Sci-Fi</div>
+                  <div className="movie-list__plot">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore quae aliquam
+                    veritatis? In corporis provident consectetur ipsa ullam, doloremque quaerat,
+                    odio eligendi non qui aperiam nisi voluptatibus suscipit, perferendis
+                    laudantium?
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div className="movie-list__info">
+                  <div className="movie-list__poster">
+                    <img
+                      src="https://demo.gloriathemes.com/noxe/demo/wp-content/uploads/2020/02/title-image-12-1255x790.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="movie-list__title">제목</div>
+                  <div className="movie-list__genre">Adventure, Drama, Sci-Fi</div>
+                  <div className="movie-list__plot">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore quae aliquam
+                    veritatis? In corporis provident consectetur ipsa ullam, doloremque quaerat,
+                    odio eligendi non qui aperiam nisi voluptatibus suscipit, perferendis
+                    laudantium?
+                  </div>
+                </div>
+              </a>
+            </li>
+          </ul>
+
           {/* 디테일페이지 */}
           <div className="mt30"></div>
           <h2 className="heading regular">Details</h2>
           <div className="mt10"></div>
+          <ul className="ratings">
+            <li>
+              <strong>Internet Movie Database</strong>
+              <div>8.4/10</div>
+            </li>
+            <li>
+              <strong>Rotten Tomatoes</strong>
+              <div>94%</div>
+            </li>
+            <li>
+              <strong>Metacritic</strong>
+              <div>78/100</div>
+            </li>
+          </ul>
+          <br />
+          <br />
+          <br />
+          <br />
+          <div className="info-block">
+            <h3 className="heading regular">Cast</h3>
+            <hr className="line-type1 mt20" aria-hidden="true" />
+            <h4 className="heading small mt10">Director</h4>
+            <p className="txt-type2">Anthony Russo, Joe Russo</p>
+          </div>
         </div>
       </main>
       <footer className="footer">
