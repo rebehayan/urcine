666b2590110307535af4901bd7078bcf65a23c03
hasungpil <dependspic@gmail.com>
2024-01-17 17:16:40 +0900

    영화개별항목리스트업

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,9 @@
 import axios from "axios";
-import React, { useCallback, useEffect, useState } from "react"; // eslint-disable-line no-unused-vars
+import React, { useEffect, useState } from "react"; // eslint-disable-line no-unused-vars
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
-  const [movie1, setMovie1] = useState([]);
-
-  const APIKEY = "https://omdbapi.com/?apikey=ef297970&s=vanilla";
-  const MOVIE1 = "https://omdbapi.com/?apikey=ef297970&i=tt0816692";
-  const MovieId = [
+  const paramId = [
     "tt1798709",
     "tt0206634",
     "tt2194499",
@@ -20,22 +16,33 @@ const MovieList = () => {
     "tt0770828",
     "tt0259711",
   ];
+  const fetchMovies = async () => {
+    try {
+      const responses = await Promise.all(
+        //11개의 객체를 모두 담은 배열로 만든다.
+        paramId.map((id) => axios.get(`https://omdbapi.com/?apikey=ef297970&i=${id}`))
+      );
+      const movieData = responses.map((response) => response.data); //. data는 json에 있는 영화정보부분의 key값
+      setMovies(movieData);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
 
-  const fetchMovies = useCallback(async () => {
-    const response = await axios.get(APIKEY);
-    setMovies(response.data.Search);
-  }, [APIKEY]);
-
-  const fetchID = useCallback(async () => {
-    const response = await axios.get(MovieId);
-    setMovie1(response.data);
-    console.log(response.data);
-  }, [MovieId]);
+  const bestId = ["tt0119643", "tt0259711"];
+  // 영화검색용
+  // const searchMovies = async () => {
+  //   try {
+  //     const responses = await axios.get(`https://omdbapi.com/?apikey=ef297970&s=madison`);
+  //     setMovies(responses.data.Search);
+  //   } catch (error) {
+  //     console.error("Error fetching movies:", error);
+  //   }
+  // };
 
   useEffect(() => {
     fetchMovies();
-    fetchID();
-  }, [fetchMovies, fetchID]);
+  });
 
   return (
     <>
@@ -49,8 +56,8 @@ const MovieList = () => {
                     <div className="movie-list__poster">
                       <img src={movie.Poster} alt="" />
                     </div>
-                    <div className="movie-list__title">{movies.Title}</div>
-                    <div className="movie-list__year">{movies.Year}</div>
+                    <div className="movie-list__title">{movie.Title}</div>
+                    <div className="movie-list__year">{movie.Year}</div>
                   </div>
                 </a>
               </li>
